5e26f161d44fbce779956817922d7f7880e3adfa
Mbouopda ndeffo <ndeffo.jugal98@gmail.com>
2019-06-10 11:40:25 +0200

    "Nothing changed"

--- src/components/myscreens/login/index.js
+++ src/components/myscreens/login/index.js
@@ -3,6 +3,183 @@ import autobind from "autobind-decorator";
 import { Content, Card, CardItem, Text, Body,Container, Header, Form, Item,Title, Input, Left,Right,Button} from "native-base";
 //import { Button,View } from "react-native";
 
+import { AsyncStorage,View,TouchableOpacity } from "react-native";
+//import { observable } from 'mobx';
+import { observer,extendObservable, inject } from "mobx-react";
+import styles from "./styles";
+import stores from "../../../stores";
+import routerActions from 'reazy-native-router-actions';
+import { functionDeclaration } from "@babel/types";
+
+import PhoneInput from 'react-native-phone-input';
+import CountryPicker from 'react-native-country-picker-modal';
+
+const loginStore = stores.loginStore;
+
+
+@observer
+export default class LoginView extends Component {
+  constructor(props){
+   super(props);
+   this.onClickContinue = this.onClickContinue.bind(this);
+   this.state = {
+       cca2: 'US',   
+       valid:'',
+       type:'',
+       value:''
+    };
+   
+ }
+
+componentDidMount() {
+  this.setState({
+    pickerData: this.phone.getPickerData(),
+  });
+}
+@autobind
+onPressFlag() {
+  this.countryPicker.openModal();
+}
+
+selectCountry(country) {
+  this.phone.selectCountry(country.cca2.toLowerCase());
+  this.setState({ cca2: country.cca2 });
+}
+
+
+@autobind
+async updateInfo() {
+
+    this.setState({
+      valid: this.phone.isValidNumber(),
+      type: this.phone.getNumberType(),
+      value: this.phone.getValue(),
+    });
+
+}
+  
+
+
+@autobind
+async onClickContinue() {
+  try {
+    await  this.updateInfo()
+
+    if (this.state.value == "") {
+      throw new Error("Please provide phone number.");
+    }
+  } catch (e) {
+    alert(e.message);
+  }
+
+  
+
+  console.warn(this.state.valid);   
+  console.warn(this.state.type);
+  console.warn(this.state.value);
+  console.warn(this.state.cca2);
+  
+       
+  
+      /*  if (loginStore.checkUser(loginStore.phoneNumber) == true){
+              this.props.navigation.navigate("SignIn")
+        }else{
+               this.props.navigation.navigate("SignUp")
+
+        }*/
+} 
+  
+
+
+
+
+
+  
+  render() {
+
+    return (
+      <Container>
+      <Content>
+         <Left/>
+      <Header style={{marginBottom:450}}>
+      <Body>
+            <Title>BleashUp </Title>
+      </Body>
+         <Right/>
+      </Header>
+
+
+        <Form style={styles.formstyle}>
+          <Header >
+            <Left/>
+            <Body>
+            <Title>Phone Number</Title>
+            </Body>
+           </Header>
+          <Right/>
+         
+          <Item style={{marginTop:5}} regular >
+          <PhoneInput
+          ref={(ref) => {
+            this.phone = ref;
+          }}
+          onChange={value => this.updateInfo()}
+          onPressFlag={this.onPressFlag}
+          value={this.state.value}
+         />
+        </Item>
+
+        <CountryPicker
+          ref={(ref) => {
+            this.countryPicker = ref;
+          }}
+          onChange={value => this.selectCountry(value)}
+          translation="eng"
+          cca2={this.state.cca2}
+        >
+          <View/>
+        </CountryPicker>
+
+        
+
+       
+
+          <Button   style={styles.buttonstyle}
+           onPress={()=>{this.onClickContinue()
+            }}
+           >
+          <Text style={{ paddingLeft:40}}> Continue </Text>
+          </Button>
+
+        </Form>
+
+
+      </Content>
+    </Container>
+
+
+    );
+  }
+  
+}
+
+
+
+
+
+
+
+
+ //console.warn(this.state.value) 
+   //console.warn(this.state.type) 
+  //console.warn(this.state.valid.toString())
+
+/**
+ * import React, { Component } from "react";
+import autobind from "autobind-decorator";
+import { Content, Card, CardItem, Text, Body,Container, Header, Form, Item,Title, Input, Left,Right,Button} from "native-base";
+//import { Button,View } from "react-native";
+
 import { AsyncStorage } from "react-native";
 //import { observable } from 'mobx';
 import { observer,extendObservable, inject } from "mobx-react";
@@ -98,18 +275,16 @@ _onPhoneNumberChanged(text) {
 
 
 
-
-
-
-
-
-
-
-
+ */
 
 
 /**
- * 
+ *         <Input placeholder="please enter phone number"  
+        onChangeText = {this._onPhoneNumberChanged} 
+        value = {this.state.phoneNumber} 
+        keyboardType={'phone-pad'} />
+
+
  *      <View>
         <Text> {loginStore.counter} </Text>
      
@@ -160,7 +335,35 @@ _onPhoneNumberChanged(text) {
 //const loginStore = stores.loginStore;
 //const loginStore = React.useContext(LoginStore);
 
-    */
+    
+
+
+@autobind
+renderInfo() {
+  if (this.state.value) {
+    return (
+      <View style={styles.info}>
+        <Text>
+          Is Valid:{" "}
+          <Text style={{ fontWeight: "bold" }}>
+            {this.state.valid.toString()}
+          </Text>
+        </Text>
+        <Text>
+          Type: <Text style={{ fontWeight: "bold" }}>{this.state.type}</Text>
+        </Text>
+        <Text>
+          Value:{" "}
+          <Text style={{ fontWeight: "bold" }}>{this.state.value}</Text>
+        </Text>
+        <Text>
+          Value:{" "}
+          <Text style={{ fontWeight: "bold" }}>{this.state.cca2}</Text>
+        </Text>
+      </View>
+    );
+  }
+}*/
 
 
 
--- src/components/myscreens/signUp/index.js
+++ src/components/myscreens/signUp/index.js
@@ -1,31 +1,380 @@
 
-import React, { Component } from "react";
-import { Content, Card, CardItem, Text, Body} from "native-base";
-import { Button,View } from "react-native";
+import React, { Component } from 'react';
+import { StyleSheet,Button,Text, TouchableOpacity , View } from 'react-native';
+import autobind from "autobind-decorator";
 
-export default class SignInView extends Component {
+import PhoneInput from 'react-native-phone-input';
+import CountryPicker from 'react-native-country-picker-modal';
+
+export default  class SignUpView extends Component {
+  constructor() {
+    super();
+
+    this.onPressFlag = this.onPressFlag.bind(this);
+    this.selectCountry = this.selectCountry.bind(this);
+    this.state = {
+      cca2: 'US',
+      country:"",
+      valid:"",
+      type:"",
+      value:""
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      pickerData: this.phone.getPickerData(),
+    });
+  }
+
+  onPressFlag() {
+    this.countryPicker.openModal();
+  }
+
+  selectCountry(country) {
+    this.phone.selectCountry(country.cca2.toLowerCase());
+    this.setState({ cca2: country.name });
+  }
+  @autobind
+  updateInfo() {
+    this.setState({
+      valid: this.phone.isValidNumber(),
+      type: this.phone.getNumberType(),
+      value: this.phone.getValue(),
+    });
+    console.warn(this.state.value) 
+    console.warn(this.state.type) 
+    console.warn(this.state.valid.toString()) 
+  }
+
+  @autobind
+   _onClickSignUp() {
+    console.warn(this.state.cca2) 
+    //console.warn(this.state.country) 
+    //console.warn(this.state.value) 
+    console.warn(this.state.valid.toString()) 
+    //console.warn(this.state.type) 
+    
+    } 
+    renderInfo() {
+      if (this.state.value) {
+        return (
+          <View style={styles.info}>
+            <Text>
+              Is Valid:{" "}
+              <Text style={{ fontWeight: "bold" }}>
+                {this.state.valid.toString()}
+              </Text>
+            </Text>
+            <Text>
+              Type: <Text style={{ fontWeight: "bold" }}>{this.state.type}</Text>
+            </Text>
+            <Text>
+              Value:{" "}
+              <Text style={{ fontWeight: "bold" }}>{this.state.value}</Text>
+            </Text>
+            <Text>
+              Value:{" "}
+              <Text style={{ fontWeight: "bold" }}>{this.state.country}</Text>
+            </Text>
+          </View>
+        );
+      }
+    }
+
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <PhoneInput
+          ref={(ref) => {
+            this.phone = ref;
+          }}
+          onChange={value => this.updateInfo}
+          onPressFlag={this.onPressFlag}
+        />
+
+        <CountryPicker
+          ref={(ref) => {
+            this.countryPicker = ref;
+          }}
+          onChange={value => this.selectCountry(value)}
+          translation="eng"
+          cca2={this.state.cca2}
+        >
+          <View />
+        </CountryPicker>
+
+        <TouchableOpacity onPress={this.updateInfo} >
+          <Text>Get Info</Text>
+        </TouchableOpacity>
+
+        <Button  onPress={this._onClickSignUp} title="SignUp">
+           
+        </Button>
+
+        {this.renderInfo()}
+
+      </View>
+    );
+  }
+}
+
+let styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 20,
+    paddingTop: 60,
+  },
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import React, { Component } from "react";
+import autobind from "autobind-decorator";
+import { Content, Card, CardItem, Text, Body,Container, Header, Form, Item,Title, Input, Left,Right,H3,Spinner,Button} from "native-base";
+//import { Button,View } from "react-native";
+
+import { AsyncStorage } from "react-native";
+import { observer,extendObservable, inject } from "mobx-react";
+import styles from "./styles";
+import stores from "../../../stores";
+import routerActions from 'reazy-native-router-actions';
+import { functionDeclaration } from "@babel/types";
+
+
+const loginStore = stores.loginStore;
+
+@observer
+export default class SignUpView extends Component {
+ 
+  constructor(props){
+    super(props);
+    this.state = {
+      name:'',
+      surname:'',
+      country:'',
+      email:'',
+      birthDate:'',  
+      password:'',
+      confirmPassword:''
+     };
+    
+  }
+
+@autobind
+_onNameChanged(text) {
+      this.setState({name: text})    
+      //console.warn(loginStore.password);
+}
+@autobind
+_onSurnameChanged(text) {
+      this.setState({surname: text})    
+      //console.warn(loginStore.password);
+}  
+@autobind
+_onCountryChanged(text) {
+      this.setState({country: text})    
+      //console.warn(loginStore.password);
+}  
+@autobind
+_onEmailChanged(text) {
+      this.setState({email: text})    
+      //console.warn(loginStore.password);
+}
+  @autobind
+_onBirthDateChanged(text) {
+      this.setState({birthDate: text})    
+      //console.warn(loginStore.password);
+}  
+@autobind
+_onPasswordChanged(text) {
+      this.setState({password: text})    
+      //console.warn(loginStore.password);
+}
+@autobind
+_onConfirmPasswordChanged(text) {
+      this.setState({confirmPassword: text})    
+      //console.warn(loginStore.password);
+}
+
+
+
+@autobind
+_onClickForgotPassword(){this.props.navigation.navigate("ForgotPassword")} 
+  
+
+  @autobind
+  async _onClickSignIn() {
+    loginStore.password = this.state.password
+    
+    try {
+        await loginStore.login();
+        if (loginStore.loading == true){
+            this.props.navigation.navigate("Home")
+        }
+    } catch (e) {
+      alert(e.message);
+    }
+
+    } 
+
+      
+
+
   render() {
    
   
     return (
 
-      <Content padder style={{ marginTop: 0 }}>
-        <Card style={{ flex: 0 }}>
-          <CardItem>
+      <Container>
+      <Content>
+      <Left/>
+      <Header style={{marginBottom:450}}>
+      <Body>
+            <Title>BleashUp </Title>
+      </Body>
+         <Right/>
+      </Header>
+
+
+        <Form style={styles.formstyle}>
+        
+         <Button transparent full onPress={this._onClickForgotPassword}
+         style={{marginBottom:0,marginTop:190,marginLeft:0}} >
+          <H3  style={{color:"green",fontSize:15,marginLeft:170}}>Forgot password?</H3>
+          </Button>
+
+          <Header style={{marginBottom:-70}}>
+            <Left/>
             <Body>
-              <Text>
-                NativeBase builds a layer on top of React Native that provides
-                you with basic set of components for mobile application
-                development. This helps you to build world-class application
-                experiences on native platforms.
-              </Text>
+            <Title style={{paddingLeft:20}}>Sign Up</Title>
             </Body>
-          </CardItem>
-        </Card>
+           </Header>
+          
+
+        <Item style={{marginTop:30}} regular >
+        <Input placeholder="Please enter name"  
+        onChangeText = {this._onNameChanged} 
+        value = {this.state.name}  />
+        </Item>
+        <Item style={{marginTop:5}} regular >
+        <Input placeholder="Please enter surname"  
+        onChangeText = {this._onSurnameChanged} 
+        value = {this.state.surname}  />
+        </Item>
+        <Item style={{marginTop:5}} regular >
+        <Input placeholder="Please enter country"  
+        onChangeText = {this._onCountryChanged} 
+        value = {this.state.country}  />
+        </Item>
+        <Item style={{marginTop:5}} regular >
+        <Input placeholder="Please enter email"  
+        onChangeText = {this._onEmailChanged} 
+        value = {this.state.email}  keyboardType="email-address"
+        autoCapitalize="none" autoCorrect={false} />
+        </Item>
+        <Item style={{marginTop:5}} regular >
+        <Input placeholder="Please enter birthDate"  
+        onChangeText = {this._onBirthDateChanged} 
+        value = {this.state.birthDate} />
+        </Item>
+        <Item style={{marginTop:5}} regular >
+        <Input placeholder="Please enter password"  
+        onChangeText = {this._onPasswordChanged} 
+        value = {this.state.password} secureTextEntry      autoCapitalize="none" autoCorrect={false}/>
+        </Item>
+        <Item style={{marginTop:5}} regular >
+        <Input placeholder="Please confirm password"  
+        onChangeText = {this._onConfirmPasswordChanged} 
+        value = {this.state.confirmPassword} secureTextEntry      autoCapitalize="none" autoCorrect={false}/>
+        </Item>
+
+
+
+
+          <Button   style={styles.buttonstyle}
+             onPress={this._onClickSignIn}>
+           <Text style={{ paddingLeft:50}} >Sign In</Text>
+           
+          </Button>
+
+        </Form>
+
       </Content>
-    );
+    </Container>
+   
+   
+   
+   
+      );
   
     }
+
   
 }
 
+*/
