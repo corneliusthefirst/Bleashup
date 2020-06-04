95485401d2a28b3405f1a5d2fe40ebe105dc8bd0
corneliusthefirst <ndeffocornelius@gmail.com>
2020-06-04 23:08:42 +0200

    resolve remind merge conflict

--- src/components/myscreens/reminds/index.js
+++ src/components/myscreens/reminds/index.js
@@ -877,33 +877,12 @@ export default class Reminds extends Component {
                 />
               </View>
 
-<<<<<<< HEAD
-              <View
-                style={{
-                  width: "10%",
-                  paddingLeft: "1%",
-                  height: "100%",
-                  justifyContent: "center",
-                }}
-              >
-                <Icon
-                  onPress={() => {
-                    this.props.openMenu();
-                  }}
-                  style={{ color: colorList.headerIcon }}
-                  type={"Ionicons"}
-                  name={"ios-menu"}
-                />
-              </View>
-            </View>
-=======
             {/*<View style={{ width: '10%', paddingLeft: '1%',height:"100%",justifyContent:"center" }}>
               <Icon onPress={() => {
                 this.props.openMenu()
               }} style={{ color: colorList.headerIcon }} type={"Ionicons"} name={"ios-menu"}></Icon>
             </View>*/}
 
->>>>>>> 2c3781b635ba27094a4a0f6196b52bfbd5caf65b
           </View>
         )}
 
