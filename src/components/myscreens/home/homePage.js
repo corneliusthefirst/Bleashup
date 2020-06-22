b3559c0a0fd89d6d082c0d1e0bc5aa5a3ccb63c5
corneliusthefirst <ndeffocornelius@gmail.com>
2020-06-22 12:39:05 +0200

    solve homePage conflict

--- src/components/myscreens/home/homePage.js
+++ src/components/myscreens/home/homePage.js
@@ -39,22 +39,10 @@ import CurrentEventView from '../currentevents';
 import emitter from '../../../services/eventEmiter';
 import firebase from 'react-native-firebase';
 import GState from '../../../stores/globalState';
-<<<<<<< HEAD
-import CreateEvent from '../event/createEvent/CreateEvent';
-
-import ForeignEventsModal from './ForeignEventsModal';
-import DeepLinking from 'react-native-deep-linking';
-import shadower from '../../shadower';
-//import TabModal from "./TabModal";
-import bleashupHeaderStyle from '../../../services/bleashupHeaderStyle';
-import Menu, { MenuItem, MenuDivider } from 'react-native-material-menu';
-import colorList from '../../colorList';
-=======
 import DeepLinking from 'react-native-deep-linking';
 import shadower from "../../shadower";
 import bleashupHeaderStyle from "../../../services/bleashupHeaderStyle";
 import colorList from "../../colorList";
->>>>>>> d84c42a9f3c25662cf721f0f8a41ef6a5137949d
 import BeNavigator from '../../../services/navigationServices';
 import { PrivacyRequester, shared_post } from '../settings/privacy/Requester';
 import EventListener from '../../../services/severEventListener';
@@ -287,15 +275,7 @@ class Home extends Component {
 
             <View style={{ height: '100%', alignSelf: 'flex-end', display: 'flex', flexDirection: 'row', marginRight: '2%' }}>
 
-<<<<<<< HEAD
-              <TouchableOpacity style={{ height: 40, alignItems: 'center', justifyContent: 'center' }} onPress={() => this.navigateToInvitations()}>
-                <Icon name="sc-telegram" active={true} type="EvilIcons" style={{ color: colorList.headerIcon }} onPress={() => this.navigateToInvitations()} />
-              </TouchableOpacity>
-
-              <TouchableOpacity style={{ height: 40, alignItems: 'center', justifyContent: 'center' }} onPress={this.settings} >
-=======
               <TouchableOpacity style={{ height: 40, alignItems: "center", justifyContent: "center" }} onPress={this.settings} >
->>>>>>> d84c42a9f3c25662cf721f0f8a41ef6a5137949d
                   <Icon name="gear" active={true} type="EvilIcons" style={{ color: colorList.headerIcon, marginLeft: width / 35 }} onPress={this.settings}/>
               </TouchableOpacity>
 
